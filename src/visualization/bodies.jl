de14e9ca86b748c103cdbb0852f03288eff05c45
Brian Jackson <bjack205@gmail.com>
2021-12-09 14:59:05 -0500

    Move visualization into module

--- src/visualization/bodies.jl
+++ src/visualization/bodies.jl
@@ -1,9 +1,27 @@
 using GeometryBasics
+using Colors
+using FixedPointNumbers
+
+const DENSITIES = Dict(
+    :aluminum => 2.7 * 1000,
+    :steel => 8.05 * 1000
+)
 
 struct CylindricalBody
     density::Float64
     radius::Float64
     length::Float64
+    color::RGB{FixedPointNumbers.N0f8}
+end
+
+function CylindricalBody(radius::Real, length::Real, mat::Symbol; kwargs...)
+    CylindricalBody(radius, length, density=DENSITIES[mat]; kwargs...)
+end
+
+function CylindricalBody(radius::Real, length::Real; 
+    density::Real=DENSITIES[:aluminum], color=colorant"blue"
+)
+    CylindricalBody(Float64(density), Float64(radius), Float64(length), color)
 end
 
 mass(body::CylindricalBody) = π*body.radius^2 * body.length * body.density
@@ -11,7 +29,7 @@ mass(body::CylindricalBody) = π*body.radius^2 * body.length * body.density
 function inertiatensor(body::CylindricalBody)
     r,h = body.radius, body.length
     m = mass(body)
-    Ixx = Iyy = m*(3&r^2 + h^2) / 12
+    Ixx = Iyy = m*(3*r^2 + h^2) / 12
     Izz = m*r^2/2
     SA[
         Ixx 0 0
@@ -23,4 +41,8 @@ end
 function geometry(body::CylindricalBody)
     h = body.length
     Cylinder(Point3(0,0,-h/2), Point3(0,0,h/2), body.radius)
+end
+
+function RigidBody(body::CylindricalBody)
+    RigidBody(mass(body), inertiatensor(body))
 end
--- src/visualization/visualization.jl
+++ src/visualization/visualization.jl
@@ -1,5 +1,40 @@
 using MeshCat, GeometryBasics, CoordinateTransformations, Rotations, Colors
 
+function visualize!(vis, model, X::Vector{<:AbstractVector}, params::SimParams)
+    anim = MeshCat.Animation(floor(Int,1/params.h))
+    for k = 1:params.N
+        atframe(anim, k) do
+            visualize!(vis, model, X[k])
+        end
+    end
+    setanimation!(vis, anim)
+end
+
+function launchvis(model::RobotArm, x0)
+    vis = Visualizer()
+    for (j, body) in enumerate(model.geom)
+        geom = geometry(body)
+        mat = MeshPhongMaterial(color=body.color)      
+        name = "body" * string(j)
+        setobject!(vis[name], geom, mat)
+        setobject!(vis[name]["com"], Triad())
+        settransform!(vis[name]["com"], LinearMap(I(3)*0.5))
+    end
+    open(vis)
+    visualize!(vis, model, x0)
+    return vis
+end
+
+function visualize!(vis, model::RobotArm, x)
+    for j = 1:model.numlinks
+        r = gettran(model, x, j)
+        q = getquat(model, x, j)
+        T = compose(Translation(r), LinearMap(UnitQuaternion(q)))
+        name = "body" * string(j)
+        settransform!(vis[name], T)
+    end
+end
+
 function launchvis(body::DoublePendulum, x0)
     vis = Visualizer()
     geom1 = Cylinder(Point(body.joint0.p2), Point(body.joint1.p1), 0.2)
@@ -28,14 +63,3 @@ function visualize!(vis, model::TwoBody, x::AbstractVector{<:Real})
     settransform!(vis["body1"], compose(Translation(r1), LinearMap(UnitQuaternion(q1))))
     settransform!(vis["body2"], compose(Translation(r2), LinearMap(UnitQuaternion(q2))))
 end
-
-function visualize!(vis, model, X::Vector{<:AbstractVector}, params::SimParams)
-    anim = MeshCat.Animation(floor(Int,1/params.h))
-    for k = 1:params.N
-        atframe(anim, k) do
-            visualize!(vis, model, X[k])
-        end
-    end
-    setanimation!(vis, anim)
-end
-
